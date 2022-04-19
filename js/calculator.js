57530830858af3c426f04e8d70c9a3c4d6efcf9a
Przemysław Szczygieł <324254@uwr.edu.pl>
2022-04-19 15:23:08 +0200

    added: calc grabbing

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,24 @@
 function draggableCalculator(){
     let calcHolder = document.querySelector(".calculatorHolder");
+    const calcBar = document.querySelector(".calc-bar");
     calcHolder.classList.toggle("calc-active");
+    
+    calcBar.addEventListener("mousedown", () => {
+        window.addEventListener("mousemove", onDrag);
+        calcBar.classList.toggle("calc-bar-grabbed");
+    });
+    
+    window.addEventListener("mouseup", () => {
+        window.removeEventListener("mousemove", onDrag);
+        calcBar.classList.toggle("calc-bar-grabbed");
+    });
+}
+function onDrag(e){
+    const calcHolder = document.querySelector(".calculatorHolder");
+    let styles = window.getComputedStyle(calcHolder);
+    let left = parseInt(styles.left);
+    let top = parseInt(styles.top);
+    
+    calcHolder.style.left = `${left + e.movementX}px`;
+    calcHolder.style.top = `${top + e.movementY}px`;
 }
